a30c905b84829640017a89146117dce21d205150
Patrick <patrickgeyer94@gmx.de>
2016-04-23 13:59:19 +0200

    Bug Fix
    dmg bug fix

--- Crank/Assets/Scripts/Enemy/CollisonDamage.cs
+++ Crank/Assets/Scripts/Enemy/CollisonDamage.cs
@@ -6,13 +6,23 @@ public class CollisonDamage : MonoBehaviour {
     public int damage = 1;
 
 
-    void OnTriggerEnte2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Enter");
             other.SendMessage("ApplyDamage", damage);
             
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Enter");
+            other.SendMessage("ApplyDamage", damage);
+
         }
     }
 }
